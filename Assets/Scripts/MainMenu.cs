7a0e75d07ebdfea106bac5fd3ca9d45922997ab5
rena0527 <minavggeil13@gmail.com>
2026-03-09 16:30:15 +0900

    로비 슬라이스 UI

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject content;
+    public float slideDuration = 0.4f;
+    public float screenWidth = 1920f;
+
+    public void MoveToLobby()
+    {
+        StartCoroutine(Slide(0));
+    }
+
+    public void MoveToTeam()
+    {
+        StartCoroutine(Slide(-screenWidth));
+    }
+
+    IEnumerator Slide(float targetX)
+    {
+        Vector3 startPos = content.transform.localPosition;
+        Vector3 endPos = new Vector3(targetX, 0, 0);
+        float elapsed = 0;
+
+        while (elapsed < slideDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / slideDuration);
+            t = t * t * (3f - 2f * t);
+
+            content.transform.localPosition = Vector3.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+        content.transform.localPosition = endPos;
+    }
+}
